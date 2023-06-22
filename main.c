64e1196e9d2e374f561d72cd0b15339c6c8bae77
Omar <omar0642624692@gmail.com>
2023-06-21 20:51:48 -0700

    main.c

--- main.c
+++ main.c
@@ -50,21 +50,15 @@ int main(int argc, char *argv[])
 			push(&stack, value);
 		}
 		else if (strcmp(opcode, "pall") == 0)
-		{
 			pall(&stack);
-		}
 		else if (strcmp(opcode, "pint") == 0)
-		{
 			pint(&stack, line_number);
-		}
 		else if (strcmp(opcode, "pop") == 0)
-		{
 			pop(&stack, line_number);
-		}
 		else if (strcmp(opcode, "swap") == 0)
-		{
 			swap(&stack, line_number);
-		}
+		else if (strcmp(opcode, "add") == 0)
+			add(&stack, line_number);
 		else
 		{
 			fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
