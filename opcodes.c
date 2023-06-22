18d516bae7285263abccf018404b4edc1838ebb3
Omar <omar0642624692@gmail.com>
2023-06-22 10:34:49 -0700

    opcodes.c

--- opcodes.c
+++ opcodes.c
@@ -11,23 +11,24 @@
 
 void push(stack_t **stack, int value)
 {
-	stack_t *new_node = malloc(sizeof(stack_t));
+	stack_t *new_node = NULL;
+
+	new_node = malloc(sizeof(stack_t));
 	if (new_node == NULL)
 	{
 		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
-
 	new_node->n = value;
 	new_node->prev = NULL;
 	new_node->next = *stack;
 
 	if (*stack != NULL)
 	{
-        (*stack)->prev = new_node;
-    }
+		(*stack)->prev = new_node;
+	}
 
-    *stack = new_node;
+	*stack = new_node;
 }
 /**
 * pall - aa
@@ -38,6 +39,7 @@ void push(stack_t **stack, int value)
 void pall(stack_t **stack)
 {
 	stack_t *current = *stack;
+
 	while (current != NULL)
 	{
 		printf("%d\n", current->n);
@@ -72,7 +74,7 @@ void pint(stack_t **stack, unsigned int line_number)
 void pop(stack_t **stack, unsigned int line_number)
 {
 	stack_t *top;
-	
+
 	if (*stack == NULL)
 	{
 		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
@@ -96,7 +98,7 @@ void pop(stack_t **stack, unsigned int line_number)
 void swap(stack_t **stack, unsigned int line_number)
 {
 	int temp;
-	
+
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
