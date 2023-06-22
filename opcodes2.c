b532046e49c7a541fd1dff77766f00dc53b41c77
Omar <omar0642624692@gmail.com>
2023-06-21 20:53:04 -0700

    opcodes2.c

---
+++ opcodes2.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+
+/**
+* add - aa
+* @stack: a
+* @line_number: aa
+* Return: nt
+*/
+
+
+void add(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	(*stack)->next->n += (*stack)->n;
+	pop(stack, line_number);
+}
