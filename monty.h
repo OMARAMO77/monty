b2bcb643b73b582c50949765f74ff383e9cfe4b9
Omar <omar0642624692@gmail.com>
2023-06-22 18:28:13 -0700

    monty.h

--- monty.h
+++ monty.h
@@ -54,5 +54,6 @@ void sub(stack_t **stack, unsigned int line_number);
 void divide(stack_t **stack, unsigned int line_number);
 void multiply(stack_t **stack, unsigned int line_number);
 void module(stack_t **stack, unsigned int line_number);
+void print_char(stack_t **stack, unsigned int line_number);
 
 #endif
