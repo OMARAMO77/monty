858613be49e507930eefc3cb2c5d4c8f0b89672c
Omar <omar0642624692@gmail.com>
2023-06-22 18:28:23 -0700

    opcodes2.c

--- opcodes2.c
+++ opcodes2.c
@@ -13,3 +13,29 @@ void nop(stack_t **stack, unsigned int line_number)
 	(void)stack;
 	(void)line_number;
 }
+
+/**
+ * print_char - aa
+ * @stack: aa
+ * @line_number: aa
+ * Return: nt
+ */
+void print_char(stack_t **stack, unsigned int line_number)
+{
+	int value;
+
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%u: can't pchar, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+    }
+
+	value = (*stack)->n;
+	if (value < 0 || value > 127)
+	{
+		fprintf(stderr, "L%u: can't pchar, value out of range\n", line_number);
+		exit(EXIT_FAILURE);
+    }
+	putchar(value);
+	putchar('\n');
+}
