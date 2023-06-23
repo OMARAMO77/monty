65d5a7fe4d0b0ceaa0bc9fd009738d726e9c7a29
Omar <omar0642624692@gmail.com>
2023-06-22 17:15:45 -0700

    select_op.c

---
+++ select_op.c
@@ -0,0 +1,31 @@
+#include "monty.h"
+
+/**
+  * select_op - selects the corresponding opcode function
+  * @str: The instruction to be executed
+  *
+  * Return: pointer to the function if the opcode exists or
+  * NULL othewise
+  */
+void (*select_op(char *str))(stack_t **, unsigned int)
+{
+	instruction_t opcs[] = {
+		{"push", push},
+		{"pall", pall},
+		{"pint", pint},
+		{"pop", pop},
+		{"swap", swap},
+		{"add", add},
+		{"nop", nop},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (opcs[i].opcode)
+	{
+		if (strcmp(str, opcs[i].opcode) == 0)
+			return (opcs[i].f);
+		++i;
+	}
+	return (NULL);
+}
