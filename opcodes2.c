9751d0581614ed4198cd1a982e32808cdf19d648
Omar <omar0642624692@gmail.com>
2023-06-22 17:48:06 -0700

    opcodes2.c

--- opcodes2.c
+++ opcodes2.c
@@ -49,3 +49,27 @@ void sub(stack_t **stack, unsigned int line_number)
 	(*stack)->next->n -= (*stack)->n;
 	pop(stack, line_number);
 }
+
+/**
+ * divide - aa
+ * @stack: aa
+ * @line_number: aa
+ * Return: nth
+ */
+void divide(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't div, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+    }
+
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%u: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+    }
+
+	(*stack)->next->n /= (*stack)->n;
+	pop(stack, line_number);
+}
