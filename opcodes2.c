1e82113b71592a8d1934eb53b44ff0ba79241957
Omar <omar0642624692@gmail.com>
2023-06-22 18:57:26 -0700

    opcodes2.c

--- opcodes2.c
+++ opcodes2.c
@@ -58,29 +58,3 @@ void print_str(stack_t **stack, unsigned int line_number)
 	}
 	putchar('\n');
 }
-#include "monty.h"
-
-/**
- * rot_to_top - aa
- * @stack: aa
- * @line_number: aa
- * Return: nt
- */
-void rot_to_top(stack_t **stack, unsigned int line_number)
-{
-	stack_t *current = *stack;
-	stack_t *last = *stack;
-	(void)line_number;
-
-	if (current == NULL || current->next == NULL)
-		return;
-
-	while (last->next != NULL)
-		last = last->next;
-
-	*stack = current->next;
-	(*stack)->prev = NULL;
-	last->next = current;
-	current->prev = last;
-	current->next = NULL;
-}
