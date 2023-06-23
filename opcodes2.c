38bdd77b712b5e3662896ae450ddbe2a7f1c793d
Omar <omar0642624692@gmail.com>
2023-06-22 17:56:57 -0700

    opcodes2.c

--- opcodes2.c
+++ opcodes2.c
@@ -73,3 +73,21 @@ void divide(stack_t **stack, unsigned int line_number)
 	(*stack)->next->n /= (*stack)->n;
 	pop(stack, line_number);
 }
+
+/**
+ * multiply - a
+ * @stack: aa
+ * @line_number: aa
+ * Return: nth
+ */
+void multiply(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't mul, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+    }
+
+	(*stack)->next->n *= (*stack)->n;
+	pop(stack, line_number);
+}
